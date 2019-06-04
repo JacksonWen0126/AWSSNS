a8cff9e8de9e4589a172f87757529daeb38a00be
JacksonWen0126 <49283813+JacksonWen0126@users.noreply.github.com>
2019-06-04 16:56:09 -0500

    Update SNS.py

--- SNS.py
+++ SNS.py
@@ -1,7 +1,7 @@
-import boto3;
+import boto3
 
-sns = boto3.client('sns');
+sns = boto3.client('sns')
 
-phone_number = '+12223334444';
+phone_number = '+12223334444'
 
-sns.publish(Message='Hello from AWS SNS',PhoneNumber=phone_number);
+sns.publish(Message='Hello from AWS SNS',PhoneNumber=phone_number)
